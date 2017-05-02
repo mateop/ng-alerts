9c09ed3d427bdb81bcbe319b507128dc307e6b72
mateop <percinic@gmail.com>
2017-05-02 09:42:11 +0200

    added angular as external

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,9 @@ const config = {
         libraryTarget: 'umd',
         umdNamedDefine: true
     },
+    externals: {
+        'angular': 'angular'
+    },
     module: {
         rules: [
             {
